ca4f464fb5149f30eb5cfb53d666c95b445e99bd
roze28 <firoze.hossain01@yahoo.com>
2022-07-27 21:00:50 +0600

    class name changed

--- src/com/roze/rotateArray/Intermediate.java
+++ src/com/roze/rotateArray/Intermediate.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /*Problem: Rotate an array of n elements to the right by k steps. For example, with n
 = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4]. How many different
 ways do you know to solve this problem?*/
-public class Solution1 {
+public class Intermediate {
     /*we can create a new array and then copy elements to the
       new array. Then change the original array by using System.arraycopy().*/
     public static void rotate(int nums[], int k) {
