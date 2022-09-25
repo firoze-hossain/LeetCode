64564a972132e4a87d349264ef52c30ffbf0018e
roze28 <firoze.hossain01@yahoo.com>
2022-09-25 22:21:20 +0600

    66. Plus One

--- src/com/leetcode/array/easy/removeDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.java
+++ src/com/leetcode/array/easy/removeDuplicatesfromSortedArray/RemoveDuplicatesfromSortedArray.java
@@ -1,4 +1,7 @@
 package com.leetcode.array.easy.removeDuplicatesfromSortedArray;
 
 public class RemoveDuplicatesfromSortedArray {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
