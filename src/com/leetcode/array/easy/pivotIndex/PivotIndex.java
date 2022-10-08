b30fc3f1a78659ba379e8a225bced44dee670a48
roze28 <firoze.hossain01@yahoo.com>
2022-10-08 19:09:33 +0600

    Find Pivot Index

--- src/com/leetcode/array/easy/pivotIndex/PivotIndex.java
+++ src/com/leetcode/array/easy/pivotIndex/PivotIndex.java
@@ -1,4 +1,26 @@
 package com.leetcode.array.easy.pivotIndex;
 
 public class PivotIndex {
+    public static int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int total = 0, leftSum = 0;
+        for (int k : nums) {
+            total = total + k;
+        }
+        for (int i = 0; i < n; i++) {
+            if (leftSum == total - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum = leftSum + nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        //int nums[] = {1, 7, 3, 6, 5, 6};
+        //int nums[]={1,2,3};
+        int nums[] = {2, 1, -1};
+        int result = pivotIndex(nums);
+        System.out.println(result);
+    }
 }
