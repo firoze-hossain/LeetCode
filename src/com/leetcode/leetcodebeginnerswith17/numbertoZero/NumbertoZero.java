d30569276cc6f1f4461392b1014029822b7b0fcf
firoze-hossain <firoze.hossain@outlook.com>
2024-12-20 22:09:15 +0600

    Number of Steps to Reduce a Number to Zero

---
+++ src/com/leetcode/leetcodebeginnerswith17/numbertoZero/NumbertoZero.java
@@ -0,0 +1,29 @@
+package com.leetcode.leetcodebeginnerswith17.numbertoZero;
+
+import java.util.Scanner;
+
+public class NumbertoZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        NumbertoZero numbertoZero = new NumbertoZero();
+        int steps = numbertoZero.numOfSteps(n);
+        System.out.println("Total steps: " + steps);
+        sc.close();
+    }
+
+    public int numOfSteps(int num) {
+        int count = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+                count++;
+            } else {
+                num -= 1;
+                count++;
+            }
+        }
+        return count;
+    }
+}
