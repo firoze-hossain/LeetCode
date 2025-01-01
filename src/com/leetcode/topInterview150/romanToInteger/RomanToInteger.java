793dc305b83f44a7b76d09da16b73a127d40e734
firoze-hossain <firoze.hossain@outlook.com>
2025-01-01 21:25:45 +0600

    Roman to Integer

---
+++ src/com/leetcode/topInterview150/romanToInteger/RomanToInteger.java
@@ -0,0 +1,32 @@
+package com.leetcode.topInterview150.romanToInteger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String roman = "MCMXCIV";
+        System.out.println(romanToInt(roman));
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        Map<Character, Integer> roman = new HashMap<>();
+        roman.put('I', 1);
+        roman.put('V', 5);
+        roman.put('X', 10);
+        roman.put('L', 50);
+        roman.put('C', 100);
+        roman.put('D', 500);
+        roman.put('M', 1000);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                result -= roman.get(s.charAt(i));
+            } else {
+                result += roman.get(s.charAt(i));
+            }
+        }
+        return result + roman.get(s.charAt(s.length() - 1));
+    }
+}
