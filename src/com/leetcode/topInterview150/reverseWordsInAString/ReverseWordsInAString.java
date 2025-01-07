5755b08e4e2ffcc2b5d03566824c0f55c9dd1385
firoze-hossain <firoze.hossain@outlook.com>
2025-01-07 21:43:59 +0600

    Reverse Words in a String

---
+++ src/com/leetcode/topInterview150/reverseWordsInAString/ReverseWordsInAString.java
@@ -0,0 +1,20 @@
+package com.leetcode.topInterview150.reverseWordsInAString;
+
+public class ReverseWordsInAString {
+    public static String reverseWords(String s) {
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+}
