fba75a6db1dbd2342b0dc9ab6e5cca2e90176b86
firoze-hossain <firoze.hossain@outlook.com>
2025-01-21 20:19:25 +0600

    Spiral Matrix

---
+++ src/com/leetcode/topInterview150/matrix/spiralMatrix/Spiral Matrix.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/spiral-matrix/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ src/com/leetcode/topInterview150/matrix/spiralMatrix/SpiralMatrix.java
@@ -0,0 +1,45 @@
+package com.leetcode.topInterview150.matrix.spiralMatrix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+
+                }
+                bottom--;
+            }
+            if (left <= right) {
+
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+
+            }
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println(spiralOrder(matrix));
+    }
+}
