ba49d052f5cc9346a2ecdc7c5d1c38d57573c27f
firoze-hossain <firoze.hossain@outlook.com>
2025-01-22 20:23:07 +0600

    Rotate Image

---
+++ src/com/leetcode/topInterview150/matrix/rotateImage/RotateImage.java
@@ -0,0 +1,29 @@
+package com.leetcode.topInterview150.matrix.rotateImage;
+
+import java.util.Arrays;
+
+public class RotateImage {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
