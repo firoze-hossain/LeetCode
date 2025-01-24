e0f2913167b79f28ad50e7efd806d1bf9c2084f0
firoze-hossain <firoze.hossain@outlook.com>
2025-01-24 19:39:16 +0600

    Game of Life

---
+++ src/com/leetcode/topInterview150/matrix/gameOfLife/Game of Life.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/game-of-life/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ src/com/leetcode/topInterview150/matrix/gameOfLife/GameOfLife.java
@@ -0,0 +1,46 @@
+package com.leetcode.topInterview150.matrix.gameOfLife;
+
+import java.util.Arrays;
+
+public class GameOfLife {
+    public static void gameOfLife(int[][] board) {
+        if (board == null && board.length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int lives = liveNeighbors(board, m, n, i, j);
+                if (board[i][j] == 1 && lives >= 2 && lives <= 3) {
+                    board[i][j] = 3;
+                }
+                if (board[i][j] == 0 && lives == 3) {
+                    board[i][j] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+
+    public static int liveNeighbors(int[][] board, int m, int n, int i, int j) {
+        int lives = 0;
+        for (int x = Math.max(i - 1, 0); x <= Math.min(i + 1, m - 1); x++) {
+            for (int y = Math.max(j - 1, 0); y <= Math.min(j + 1, n - 1); y++) {
+                lives += board[x][y] & 1;
+            }
+        }
+        lives -= board[i][j] & 1;
+        return lives;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
+        gameOfLife(board);
+        System.out.println(Arrays.deepToString(board));
+    }
+}
