ad3e7da7fb7f4ca5dd77c262d7df8bd40c94553d
firoze-hossain <firoze.hossain@outlook.com>
2025-02-03 20:18:55 +0600

    Group Anagrams

---
+++ src/com/leetcode/topInterview150/hashmap/groupAnagrams/Group Anagrams.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/group-anagrams/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ src/com/leetcode/topInterview150/hashmap/groupAnagrams/GroupAnagrams.java
@@ -0,0 +1,24 @@
+package com.leetcode.topInterview150.hashmap.groupAnagrams;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
