5c9515666639192e08d7686c9eca12edb10db575
firoze-hossain <firoze.hossain@outlook.com>
2025-02-10 20:24:05 +0600

    Valid Parentheses

---
+++ src/com/leetcode/topInterview150/stack/validParentheses/Valid Parentheses.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/valid-parentheses/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ src/com/leetcode/topInterview150/stack/validParentheses/ValidParentheses.java
@@ -0,0 +1,30 @@
+package com.leetcode.topInterview150.stack.validParentheses;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        for (char c : s.toCharArray()) {
+            if (map.containsValue(c)) {
+                stack.push(c);
+            } else if (map.containsKey(c)) {
+                if (stack.isEmpty() || map.get(c) != stack.pop()) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+}
