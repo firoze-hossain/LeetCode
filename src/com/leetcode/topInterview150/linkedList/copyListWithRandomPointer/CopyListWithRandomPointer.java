10e4e800c79188d9aec86e88e9e3384e2032369c
firoze-hossain <firoze.hossain@outlook.com>
2025-02-25 22:06:02 +0600

    Copy List with Random Pointer

---
+++ src/com/leetcode/topInterview150/linkedList/copyListWithRandomPointer/CopyListWithRandomPointer.java
@@ -0,0 +1,36 @@
+package com.leetcode.topInterview150.linkedList.copyListWithRandomPointer;
+
+import java.util.HashMap;
+
+public class CopyListWithRandomPointer {
+    public static Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        HashMap<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
