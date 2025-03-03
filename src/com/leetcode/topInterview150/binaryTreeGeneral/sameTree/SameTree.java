496eeca1fecd5f297cf5926dc493f4ded543cabd
firoze-hossain <firoze.hossain@outlook.com>
2025-03-04 05:18:39 +0600

    Same Tree

---
+++ src/com/leetcode/topInterview150/binaryTreeGeneral/sameTree/SameTree.java
@@ -0,0 +1,32 @@
+package com.leetcode.topInterview150.binaryTreeGeneral.sameTree;
+
+public class SameTree {
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
