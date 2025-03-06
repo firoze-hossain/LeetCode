83e8159969c4b668a6e228d68f6acf9e28dcd79b
firoze-hossain <firoze.hossain@outlook.com>
2025-03-06 21:29:13 +0600

    Symmetric Tree

---
+++ src/com/leetcode/topInterview150/binaryTreeGeneral/symmetricTree/Symmetric Tree.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/symmetric-tree/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ src/com/leetcode/topInterview150/binaryTreeGeneral/symmetricTree/SymmetricTree.java
@@ -0,0 +1,39 @@
+package com.leetcode.topInterview150.binaryTreeGeneral.symmetricTree;
+
+public class SymmetricTree {
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private static boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        return (node1.val == node2.val) && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
