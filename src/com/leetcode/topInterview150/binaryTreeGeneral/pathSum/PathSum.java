ec5ebcdb49e2436dc885cf6b131b5bc546b55d5a
firoze-hossain <firoze.hossain@outlook.com>
2025-03-07 22:06:16 +0600

    Path Sum

---
+++ src/com/leetcode/topInterview150/binaryTreeGeneral/pathSum/PathSum.java
@@ -0,0 +1,34 @@
+package com.leetcode.topInterview150.binaryTreeGeneral.pathSum;
+
+public class PathSum {
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
+        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+        return leftSum || rightSum;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
